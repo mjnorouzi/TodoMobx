c82b2bc73a2810ca22a8f96b4172703484cabea7
mjnorouzi <norouzi.mj78@gmail.com>
2019-02-13 18:51:20 +0330

    Updates

---
+++ precache-manifest.0d7ac0a7274efa569edcefcc30df4dc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "71edd2de4c8b01b7cd9f",
+    "url": "/TodoMobx/static/js/runtime~main.71edd2de.js"
+  },
+  {
+    "revision": "b4b2124683e3d61e48c3",
+    "url": "/TodoMobx/static/js/main.b4b21246.chunk.js"
+  },
+  {
+    "revision": "bf8d91d0c0a0b18e349e",
+    "url": "/TodoMobx/static/js/2.bf8d91d0.chunk.js"
+  },
+  {
+    "revision": "b4b2124683e3d61e48c3",
+    "url": "/TodoMobx/static/css/main.e5e1f74d.chunk.css"
+  },
+  {
+    "revision": "230b572fc7e9308a0c766a5d5e419f61",
+    "url": "/TodoMobx/index.html"
+  }
+];
